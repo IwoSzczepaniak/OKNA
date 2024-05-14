d592c29ed6a403c919816d69e025dce3eb6a9a6b
Iwo Szczepaniak <iszczepaniak@student.agh.edu.pl>
2024-05-14 20:51:52 +0200

    hw_calculations_and_charts

---
+++ hw2/lu_decompose/run_all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function run_all() {
+    arr=("$@")
+    for i in "${arr[@]}"; do
+        version="LU_decompose_${i}"
+        make run VERSION="$version"
+    done
+}
+
+declare -a versions_ids=("1" "2" "3" "4" "5" "6" "7" "8")
+
+echo ">>>>> all"
+run_all "${versions_ids[@]}"
